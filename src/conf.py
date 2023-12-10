164aaa3abab8f05684b381af1eb8fb1a9594d9fa
wbrunette <wbrunette@gmail.com>
2023-12-10 15:59:18 -0500

    create staging area for testing docs

--- src/conf.py
+++ src/conf.py
@@ -260,7 +260,7 @@ epub_exclude_files = ['search.html']
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     # 'py': ('https://docs.python.org/', None),
-    'odkx': ('https://docs.odk-x.org/', None)
+    'odkx': ('https://docs-staging.odk-x.org/', None)
 }
 
 # Add custom CSS
